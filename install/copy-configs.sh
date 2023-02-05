2f8002b885d63ef42e55a7e5d8adb71c3de9dbb2
constellationz <t.angley@protonmail.com>
2023-02-05 18:01:57 -0500

    move to copy-configs

---
+++ install/copy-configs.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/bash
+# Installs themes
+
+# Used to store certain states
+mkdir -p $HOME/.config
+mkdir -p $HOME/.config/papirus-folders
+
+# Set gtk themes
+gsettings set org.gnome.desktop.interface gtk-theme "Materia-dark-compact"
+gsettings set org.gnome.desktop.interface icon-theme "Papirus-Dark"
+gsettings set org.gnome.desktop.interface font-name "Fira Code Medium 10"
+gsettings set org.gnome.desktop.interface document-font-name "Fira Code Medium 10"
+gsettings set org.gnome.desktop.interface monospace-font-name "Fira Code Medium 10"
+
+# Change papirus folder color
+if [ ! -f "$HOME/.config/papirus-folders/theme-updated" ]; then
+	cd /tmp/
+	git clone https://github.com/PapirusDevelopmentTeam/papirus-folders.git
+	cd papirus-folders
+	git pull
+	./papirus-folders -C black --theme Papirus-Dark
+	touch $HOME/.config/papirus-folders/theme-updated
+fi
+
+# Copy configs to config directory
+SCRIPT_DIR=$(dirname $0)
+CONFIG_DIR=$SCRIPT_DIR/../config
+
+# $1 - From directory
+# $2 - To directory
+sync () {
+	# Sync new file
+	rsync -ar $1 $2
+	rename 'dot-' '.' $2*
+}
+
+for config in $(ls -A $CONFIG_DIR); do
+	echo "Syncing config/$config"
+	sync $CONFIG_DIR/$config $HOME/.config/
+done
+
+# Set wallpaper if it hasn't been set already
+if [ ! -f "$HOME/.wallpaper" ]; then
+	echo "Setting wallpaper for the first time..."	
+	$SCRIPT_DIR/set-wallpaper.sh /usr/share/backgrounds/sway/Sway_Wallpaper_Blue_2048x1536.png
+fi
+
+echo "Done!"
