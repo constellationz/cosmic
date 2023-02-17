02af6d38e652984adf570af3d251f3d3d6dc702e
constellationz <t.angley@protonmail.com>
2023-02-17 15:44:31 -0500

    update displays

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -5,6 +5,6 @@ polybar-msg cmd quit
 
 # Launch bar
 echo "---" | tee -a /tmp/polybar.log
-polybar >&1 | tee -a /tmp/polybar.log & disown
+MONITOR=DP-1 polybar example >&1 | tee -a /tmp/polybar.log & disown
 
 echo "Launched polybar"
