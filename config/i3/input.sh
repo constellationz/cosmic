f1b7e02123374683bedcd3f93a126c9c4d4be69f
constellationz <t.angley@protonmail.com>
2023-02-18 02:56:02 -0500

    increase mouse sensitivity

--- config/i3/input.sh
+++ config/i3/input.sh
@@ -3,11 +3,10 @@
 
 # Get mouse id to change
 MOUSEID=$(xinput | grep "Logitech G305" | grep "pointer" | sed -E 's/.*id=([0-9]*).*$/\1/')
-echo $MOUSEID
 
 # Set mouse sensitivity
 # Use `xinput` to list input devices
-xinput --set-prop $MOUSEID 'libinput Accel Speed' -1
+xinput --set-prop $MOUSEID 'libinput Accel Speed' -0.75
 
 # Keyboard repeat rate
 xset r rate 200 30
