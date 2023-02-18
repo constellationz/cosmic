095338d0938e27996fa2ebefe3cbe908f4d1dcb7
constellationz <t.angley@protonmail.com>
2023-02-18 04:31:10 -0500

    update dump screenshot script

--- config/i3/dumpss.sh
+++ config/i3/dumpss.sh
@@ -1,13 +1,23 @@
 #!/usr/bin/bash
 # Dump last screenshot from /tmp/screenshot.png
+# Notify user when screenshot is dumped or if not possible
 
+SSDIR="`xdg-user-dir DESKTOP`/Screenshots"
+SSNAME="`date -u +%Y_%m_%d_%H_%M_%S_%N`.png"
+
+mkdir -p $SSDIR
+
+# If there is no previous screenshot, warn user and exit
 if ! [ -a /tmp/screenshot.png ]; then
 	notify-send -u critical "No screenshot to dump"
 	exit
 fi
 
-TO="`xdg-user-dir DESKTOP`/`date -u +%Y_%m_%d_%H_%M_%S_%N`.png"
+TO="$SSDIR/$SSNAME"
 
 cp /tmp/screenshot.png "$TO"
 
 notify-send "Dumped last screenshot to $TO"
+
+# Remove screenshot that was just dumped
+rm /tmp/screenshot.png
